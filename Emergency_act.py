cec2f4b9b6ce9790d82d2b0125b01a26aff6f24b
TejaswiniBonam <130749202+TejaswiniBonam@users.noreply.github.com>
2025-02-03 17:27:31 +0530

    Update Emergency_act.py

--- Emergency_act.py
+++ Emergency_act.py
@@ -1,12 +1,13 @@
 from twilio.rest import Client
-account_sid = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx'
-auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+#account_sid = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx'
+#auth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
   from_='whatsapp:+14155238886',
   to='whatsapp:+919951280286',
-  body= 'EMERGENCY AT XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  body= 'EMERGENCY AT House No 123 Main Street, Anytown, CA 12345',
   
 )
 
